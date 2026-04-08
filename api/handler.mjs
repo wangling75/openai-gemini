54d541b90039a48ec1ade6bc1d371266fb04258e
wangling75 <513448188@qq.com>
2026-04-08 13:16:46 +0800

    Update handler.mjs

--- api/handler.mjs
+++ api/handler.mjs
@@ -3,7 +3,7 @@ import worker from "../src/worker.mjs";
 export default worker.fetch;
 
 export const config = {
-  runtime: "edge", 
+  runtime: "nodejs", 
   // Available languages and regions for Google AI Studio and Gemini API
   // https://ai.google.dev/gemini-api/docs/available-regions#available_regions
   // Vercel Edge Network Regions
